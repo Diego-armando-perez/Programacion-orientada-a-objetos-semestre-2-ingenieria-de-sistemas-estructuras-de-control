a5ef5bfe7a2b5b3732dd933a9ac7f7d80e0175f1
Diego-armando-perez <diperezs@unal.edu.co>
2025-09-02 17:17:08 -0500

    Add files via upload

---
+++ Estructuras de control practica.py
@@ -0,0 +1,63 @@
+# Ejercicio 1
+
+
+print("Hola! Conozco como imprimir frases en python")
+
+
+# Ejercicio 2
+
+
+print("Este es un numero entero = 2091")
+
+
+# Ejercicio 3
+
+
+print("Este es un numero decimal = 23,123")
+
+
+# Ejercicio 4
+
+
+c = 1234
+d = 532
+
+r = c + d
+
+print("La suma de " + str(c) + " + " + str (d) + " es: " + str(r))
+
+
+# Ejercicio 5
+
+
+r = c - d
+
+print("La resta de " + str(c) + " y " + str(d) + " es: " + str(r))
+
+# Ejercicio 6
+
+r = c * d
+
+print("La multiplicacion de " + str(c) + " y " + str(d) + " es: " + str(r))
+
+
+# Ejercicio 7
+
+
+r = c / d
+
+print("La division de " + str(c) + " y " + str(d) + " es: " + str(r))
+
+
+# Ejercicio 8
+
+
+for i in range (1, 4):
+    print (i)
+
+
+# Ejercicio 9
+
+
+for i in range (1, 10):
+    print (i)
